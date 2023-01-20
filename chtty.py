a0c69c5598e1bf24112a86be6b316b070614412e
rjgriggs <39922220+rjgriggs@users.noreply.github.com>
2023-01-20 14:10:43 -0800

    Create chtty.py
    added first file

---
+++ chtty.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load the ChatGPT model
+chatbot = pipeline("text-generation", model="microsoft/DialoGPT-medium")
+
+st.title("ChatGPT")
+
+# Create a text input for the user to type their message
+message = st.text_input("Enter your message:")
+
+# If the user enters a message and hits enter, generate a response
+if message:
+    response = chatbot(message, max_length=50)
+    st.write("ChatGPT: ", response[0]["generated_text"])
