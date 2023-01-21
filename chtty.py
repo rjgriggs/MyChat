84b2bc99f958caf23c234add1815985d525a0516
rjgriggs <39922220+rjgriggs@users.noreply.github.com>
2023-01-20 18:10:36 -0800

    Update chtty.py

--- chtty.py
+++ chtty.py
@@ -9,4 +9,11 @@ st.write("Hey there big daddy")
 st.header("testing this app")
 articleTXT = st.text_area("enter the text you want me to look at")
 
-
+if len(articleTXT) > 10:
+  if st.button("generate results"):
+    response = openai.Completion.create(
+      engine = "text-davinci-003"
+      prompt = "can you give me the response now? :" + articleTXT
+      max_token = 516,
+      
+    )
