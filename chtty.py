3d53056a84ccf000083f0d80f5b0440d19f74b24
rjgriggs <39922220+rjgriggs@users.noreply.github.com>
2023-01-20 18:17:23 -0800

    Update chtty.py

--- chtty.py
+++ chtty.py
@@ -14,7 +14,7 @@ if len(articleTXT) > 10:
     response = openai.Completion.create(
       engine = "text-davinci-003",
       prompt = "can you give me the response now? : " + articleTXT,
-      max_token = 516
+      max_token = 516,
       temperature = temp 
       
     )
