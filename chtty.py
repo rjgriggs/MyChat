91134c1f6352446d860a2bddc9e2fb41fa8ff85f
rjgriggs <39922220+rjgriggs@users.noreply.github.com>
2023-01-20 19:05:30 -0800

    Update chtty.py

--- chtty.py
+++ chtty.py
@@ -3,7 +3,7 @@ import os
 import openai
 import streamlit as st
 
-openai.api.key = st.secrets['testkey']
+openai.api.key = st.secrets["testkey"]
 # api = "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"
 st.write("Hey there big daddy")
 st.header("testing this app")
