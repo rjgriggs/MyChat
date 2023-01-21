adcd2c10df83915aabf57a2d7d4843e7d1679f0a
rjgriggs <39922220+rjgriggs@users.noreply.github.com>
2023-01-20 16:07:04 -0800

    Update chtty.py

--- chtty.py
+++ chtty.py
@@ -7,17 +7,17 @@ import streamlit as st
 
 openai.api_key = st.secrets["pass"]
 # Create Text Area Widget to enable user to enter texts
-article_text = st.text_area(“Enter your scientific texts to summarize”)
+article_text = st.text_area("Enter your scientific texts to summarize")
 # Create Radio Buttons
-output_size = st.radio( label = “What kind of output do you want?”, 
-                        options= [“To-The-Point”, “Concise”, “Detailed”]
+output_size = st.radio( label = "What kind of output do you want?", 
+                        options= ["To-The-Point”, “Concise”, “Detailed"]
                      )
 # First, we'll use an if statement to determine the desired output size 
 # and set the out_token variable accordingly:
 
-if output_size == “To-The-Point”:
+if output_size == "To-The-Point":
  out_token = 50
-elif output_size == “Concise”:
+elif output_size == "Concise":
  out_token = 128
 else:
  out_token = 516
@@ -30,7 +30,7 @@ if len(article_text)>100:
  # .......
 else:
  st.warning("Not enough words to summarize!")
-if st.button(“Generate Summary”,type=’primary’):
+if st.button("Generate Summary",type=’primary’):
  
 # Use GPT-3 to generate a summary of the article
  response = openai.Completion.create(
@@ -40,8 +40,8 @@ if st.button(“Generate Summary”,type=’primary’):
                     temperature = 0.5)
  
 # Print the generated summary
- res = response[“choices”][0][“text”]
+ res = response["choices"][0]["text"]
  st.success(res)
 
 # Give user the option to download result
-st.download_button(‘Download result’, res)
+st.download_button('Download result', res)
