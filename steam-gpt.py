7ea29e06a5a705198ede382acd1912d084ba450c
rjgriggs <39922220+rjgriggs@users.noreply.github.com>
2023-01-22 06:51:18 -0800

    Update steam-gpt.py

--- steam-gpt.py
+++ steam-gpt.py
@@ -12,10 +12,32 @@ def generate_response(prompt):
     max_tokens = 1024,
     n = 1,
     stop = none,
-    temparature = 0.5,
+    temperature = 0.5,
     
   )
   message = completions.choices[0].text
   return message
 st.title("chatbot : Streamlit + openai")
+if 'generated' not in st.sessioin_state:
+  st.session_state['generated'] = []
+  
+if 'past' not in st.session_state:
+  st.session_state['past']
+  
+def get_text():
+  input_text = st.text_input("you: ","Hello, how are you?", key="input")
+  return input_text
+  
+user_input = get_text()
     
+if user_input:
+  output = generated_response(user_input)
+  # store the output
+  st.session_state.past.append(user_input)
+  st.session_state.generated.append(output)
+  
+if st.session_state['generated']:
+  
+  for i in range(len(st.session_state['generated']) -1, -1, -1):
+    message(st.session_state["generated"][i], key=str(i))
+    message(st.session_state['past'][i], is_user = True, key=str(i) + '_user')
