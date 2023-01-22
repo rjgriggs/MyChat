9c306d8790888dde74573d5e1f4fe7661f3cba8d
rjgriggs <39922220+rjgriggs@users.noreply.github.com>
2023-01-21 16:43:21 -0800

    Update steam-gpt.py

--- steam-gpt.py
+++ steam-gpt.py
@@ -4,19 +4,17 @@ import openai
 
 openai.api.key = st.secrets["pass"]
 # openai.api_key = "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"
-st.write("Hey there big daddy")
-st.header("testing this app")
-articleTXT = st.text_area("enter the text you want me to look at")
-temp = st.slider("temparature", 0.0,1.0, 0.5)
-if len(articleTXT) > 10:
-  if st.button("generate results"):
-    response = openai.Completion.create(
-      engine = "text-davinci-003",
-      prompt = "can you give me the response now? : " + articleTXT,
-      max_token = 516,
-      temperature = temp 
-      
-    )
-    # print the out generated
-    res = response["choice3s"][0]["text"]
-    st.info(res)
+def generate_response(prompt):
+  completions = openai.Completion.create(
+    engine = "text-davinci-003",
+    prompt = prompt,
+    max_tokens = 1024,
+    n = 1,
+    stop = none,
+    temparature = 0.5,
+    
+  )
+  message = completions.choices[0].text
+  return message
+st.title("chatbot : Streamlit + openai")
+    
